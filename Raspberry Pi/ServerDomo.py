2a0494594f8d8c15056de6db6ef377cb7e77c078
Linard <linard.gauthier@gmail.com>
2016-03-03 11:09:54 +0000

    wip - Communication RPi, serveur web

--- Raspberry Pi/ServerDomo.py
+++ Raspberry Pi/ServerDomo.py
@@ -4,6 +4,8 @@
 import socket
 import threading
 import main
+import RPi.GPIO as GPIO
+import param
 
 
 class ClientThread(threading.Thread):
@@ -21,29 +23,51 @@ class ClientThread(threading.Thread):
 
         print("Connection de %s %s" % (self.ip, self.port,))
 
-        r = self.clientsocket.recv(2048).upper()
+        r = self.clientsocket.recv(2048).upper().decode("utf-8")
+        r = r.split(" ")
         print("Execution de : ", r, "...")
         # fp = open(r, 'rb')
         # self.clientsocket.send(fp.read())
-        if r == "INTERNET":
+        if r[0] == "INTERNET":
             print(self.myHouse.isInternetOn())
 
         # Command: SET_LAMP [status]
-        elif r.startswith("LAMP"):
-            status = r.split(" ")[1]
+        elif r[0] == "LAMP":
+            status = r[1]
             self.myHouse.setLamp(int(status))
 
         # Command: SET_TEMP [room] [value]
-        elif r.startswith("SET_TEMP"):
-            room = r.split(" ")[1]
-            value = r.split(" ")[2]
+        elif r[0] == "SET_TEMP":
+            room = r[1]
+            value = r[2]
             self.myHouse.setTemp(room, int(value))
 
-        # Commend: SET_MODE [room] [value]
-        elif r.startswith("SET_MODE"):
-            room = r.split(" ")[1]
-            value = r.split(" ")[2]
+        # Command: SET_MODE [room] [value]
+        elif r[0] == "SET_MODE":
+            room = r[1]
+            value = r[2]
             self.myHouse.setMode(room, value)
+
+        # Command: GET_WINDOW [room]
+        elif r[0] == "GET_WINDOW":
+            room = r[1]
+            msg = GPIO.input(param.GPIO['Windows'][room][1])
+            self.clientsocket.send(bytes(str(msg), 'UTF-8'))
+            self.clientsocket.close()
+
+        # Command: GET_DOOR [room]
+        elif r[0] == "GET_DOOR":
+            room = r[1]
+            msg = GPIO.input(param.GPIO['Doors'][room][1]
+            self.clientsocket.send(bytes(str(msg), 'UTF-8'))
+            self.clientsocket.close()
+
+       # Command: GET_TEMP [room]
+       elif r[0] == 'GET_TEMP':
+            room = r[1]
+            msg = GPIO.input(param.GPIO['Temp'][1])
+            self.clientsocket.send(bytes(str(msg), 'UTF-8'))
+            self.clientsocket.close()
 
         # print("Commander le chauffage: %s", nameHeating, " Status : %s", status)
         print("Client déconnecté...")
