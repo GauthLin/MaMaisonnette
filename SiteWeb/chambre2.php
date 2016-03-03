edf533d7f4e2cf8f32810826390d93d2b636abd5
Gaëtan WAUTIER <14258@ecam.be>
2016-03-03 12:03:00 +0100

    Fuck it

--- SiteWeb/chambre2.php
+++ SiteWeb/chambre2.php
@@ -7,4 +7,70 @@
     </head>
     <body>
 
-        <?php include("header.php"); ?>
+        <?php 	include("header.php");
+				include("functions.php");
+		if (isset ($_POST ["temp_chamb1"])){
+			$temp_1 = $_POST ["temp_chamb1"];
+			$temp_2 = $_POST ["temp_chamb2"];
+			$temp_3 = $_POST ["temp_chamb3"];
+			
+			if (!sendCommandToRPi ("SET_TEMP A ". $temp_1)){
+			echo "connect failed";
+			}
+			if (!sendCommandToRPi ("SET_TEMP B ". $temp_2)){
+				echo "connect failed";
+			}
+			if (!sendCommandToRPi ("SET_TEMP C ". $temp_3)){
+				echo "connect failed";
+			}
+			if (!sendCommandToRPi ("SET_MODE ". $_POST['Mode']." A")){
+				echo "connect failed";
+			}
+		}
+		// reception des température de la maison
+		$temp_1_RPI= getinfoToRPi("GET_TEMP A");
+		$temp_2_RPI= getinfoToRPi("GET_TEMP B");
+		$temp_3_RPI= getinfoToRPi("GET_TEMP C");
+		
+		// reception des fenetre et porte de la maison 
+		
+		echo '<form action="chambre2.php" method="post">
+		<label for="Temp_1">Température chambre 1: </label><input type="number" value="' . $temp_1_RPI . '" min="15" name="temp_chamb1"><br>
+		<label for="Temp_2">Température chambre 2: </label><input type="number" value="' . $temp_2_RPI . '" min="15" name="temp_chamb2"><br>
+		<label for="Temp_2">Température chambre 3: </label><input type="number" value="' . $temp_3_RPI . '" min="15" name="temp_chamb3"><br>
+		<label for="mode"> Mode de fonctionnement
+		<select name="Mode">
+		';
+		if (isset ($_POST ["Mode"]) && $_POST['Mode']=="AUTO")
+		{
+			echo'<option value="AUTO" selected>AUTO</option>';
+		}
+		else
+		{
+			echo'<option value="AUTO">AUTO</option>';
+		}
+		if (isset ($_POST ["Mode"]) && $_POST['Mode']=="ON")
+		{
+			echo'<option value="ON" selected>ON</option>';
+		}
+		else
+		{
+			echo'<option value="ON">ON</option>';
+		}
+		if (isset ($_POST ["Mode"]) && $_POST ['Mode']=="OFF")
+		{
+			echo'<option value="OFF" selected>OFF</option>';
+		}
+		else
+		{
+			echo'<option value="OFF">OFF</option>';
+		}
+		
+		echo'
+		</select>
+		<br>
+		<input type="submit" value= "ok">
+		</form>
+		';
+		?>
+	
--- SiteWeb/functions.php
+++ SiteWeb/functions.php
@@ -9,12 +9,12 @@ function sendCommandToRPi($command) {
 	}
 
 	//Connect socket to remote server
-	if(!socket_connect($sock , '172.17.10.60' , 1111))
+	if(!socket_connect($sock , '172.17.10.87' , 1111))
 	{
 	    $errorcode = socket_last_error();
 	    $errormsg = socket_strerror($errorcode);
 
-	    die("Could not connect: [$errorcode] $errormsg \n");
+	    return(False);
 	}
 
 	//Send the message to the server
@@ -23,8 +23,44 @@ function sendCommandToRPi($command) {
 	    $errorcode = socket_last_error();
 	    $errormsg = socket_strerror($errorcode);
 
-	    die("Could not send data: [$errorcode] $errormsg \n");
+	    return(False);
 	}
 
-	return true;	
+	return true;
+}
+
+function getinfoToRPi($command) {
+	
+	if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
+	{
+	    $errorcode = socket_last_error();
+	    $errormsg = socket_strerror($errorcode);
+
+	    die("Couldn't create socket: [$errorcode] $errormsg \n");
+	}
+
+	//Connect socket to remote server
+	if(!socket_connect($sock , '172.17.10.87' , 1111))
+	{
+	    $errorcode = socket_last_error();
+	    $errormsg = socket_strerror($errorcode);
+
+	    return(False);
+	}
+
+	//Send the message to the server
+	if( ! socket_write ( $sock , $command , strlen($command)))
+	{
+	    $errorcode = socket_last_error();
+	    $errormsg = socket_strerror($errorcode);
+
+	    return(False);
+	}
+	$buff="";
+	if (False !==($bytes=socket_recv($sock,$buff,10,MSG_WAITALL)))
+	{
+		return $buff;
+	}
+
+	return False;
 }
