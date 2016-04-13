4be742b791ea05a881cd780f861909fe96bbd998
BenjaminAiglon <Aiglon.b@hotmail.com>
2016-04-13 12:13:26 +0200

    Site web + menu

--- SiteWeb/header.php
+++ SiteWeb/header.php
@@ -1,5 +1,6 @@
        <div id="header">
 
-            <h2><img src="Images/image_maison.png" alt="Accueil" align=left hspace="5px" width="55" /> Application domotique</h2> 
+            <h2><img src="Images/image_maison.png" alt="Accueil" align=left hspace="5px" width="55" /> Application domotique <a href="View.php" class="Menu">Menu</a></h2> 
+
             
         </div>
